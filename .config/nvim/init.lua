7cc75b78e6484f3282d9c6fc21c32b50fbc38712
Hou Rui <hourui@realtek-sg.com>
2025-04-09 15:53:31 +0800

    update neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,6 @@
 -- bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system {
     'git', 'clone', '--filter=blob:none',
     'https://github.com/folke/lazy.nvim.git', '--branch=stable',
@@ -14,6 +14,12 @@ local autocmd = vim.api.nvim_create_autocmd
 local keymap = vim.keymap.set
 
 local function noremap(mode, lhs, rhs)
+  if type(lhs) == 'table' then
+    for _, key in pairs(lhs) do
+      noremap(mode, key, rhs)
+    end
+    return
+  end
   keymap(mode, lhs, rhs, {noremap = true})
 end
 
@@ -26,6 +32,11 @@ local function file_assoc(pattern, filetype)
     pattern = pattern,
     command = 'set filetype=' .. filetype,
   })
+end
+
+local function is_not_large_file()
+  local threshold = 100 * 1024 * 1024 -- 100 MB
+  return vim.fn.getfsize(vim.fn.expand('%')) < threshold
 end
 
 -- general settings
@@ -80,10 +91,8 @@ file_assoc('*.kvconfig', 'ini')
 
 -- common Emacs-like editor hotkeys
 -- copy
-noremap('n', '<C-c>', '"+yy')
-noremap('v', '<C-c>', '"+y')
-noremap('n', '<C-S-c>', '"+yy')
-noremap('v', '<C-S-c>', '"+y')
+noremap('n', {'<C-c>', '<C-S-c>'}, '"+yy')
+noremap('v', {'<C-c>', '<C-S-c>'}, '"+y')
 -- cut
 noremap('n', '<C-x>', 'dd')
 noremap('v', '<C-x>', '"+d')
@@ -96,7 +105,8 @@ noremap('i', '<C-a>', '<esc>^i')
 noremap({'n', 'v'}, '<C-e>', '$')
 noremap('i', '<C-e>', '<esc>$a')
 -- delete word
-noremap({'n', 'v'}, '<C-w>', 'diw')
+noremap({'n', 'v'}, {'<C-w>', '<C-h>', '<C-bs>'}, 'bdW')
+noremap('i', {'<C-w>', '<C-h>', '<C-bs>'}, '<esc>bdWa')
 -- save
 noremap_all('<C-s>', '<cmd>wa<cr>')
 -- quit
@@ -106,8 +116,7 @@ noremap('n', '<C-f>', 'f')
 noremap('v', '<C-f>', [[y/\V<C-R>=escape(@",'/\')<cr><cr>]])
 -- undo / redo
 noremap_all('<C-z>', '<cmd>undo<cr>')
-noremap_all('<C-y>', '<cmd>redo<cr>')
-noremap_all('<C-S-z>', '<cmd>redo<cr>')
+noremap_all({'<C-y>', '<C-S-z>'}, '<cmd>redo<cr>')
 -- backspace
 noremap('n', '<bs>', '"_dd')
 noremap('v', '<bs>', '"_d')
@@ -118,8 +127,13 @@ noremap('v', '<S-Tab>', '<gv')
 noremap('n', '<S-Tab>', '<<')
 noremap('i', '<S-Tab>', '<C-d>')
 -- swap lines
-noremap({'i', 'n'}, '<A-Up>', 'ddkkp')
-noremap({'i', 'n'}, '<A-Down>', 'ddp')
+noremap('n', '<A-Up>', 'ddkP')
+noremap('i', '<A-Up>', '<esc>ddkP')
+noremap('n', '<A-Down>', 'ddp')
+noremap('i', '<A-Down>', '<esc>ddp')
+-- select all
+noremap('n', '<A-a>', 'gg^vG$')
+noremap({'i', 'x', 'v'}, '<A-a>', '<esc>gg^vG$')
 
 -- set up plugins
 local lazy = require('lazy')
@@ -127,25 +141,33 @@ lazy.setup(
   { -- plugin list
     -- color theme
     {
-      'Hou-Rui/ayu-vim',
-      lazy = false,
-      priority = 1000,
-      init = function()
-        vim.g.ayucolor = 'minimal'
-        vim.cmd.colorscheme('ayu')
+      'Shatur/neovim-ayu',
+      config = function()
+        local ayu = require('ayu')
+        local bg_overrides = {}
+        for _, hi in pairs {
+          'Normal', 'NormalFloat', 'ColorColumn', 'SignColumn',
+          'Folded', 'FoldColumn', 'CursorLine', 'CursorColumn',
+          'CursorLine', 'CursorColumn', 'VertSplit', 'WinSeparator'
+        } do
+          bg_overrides[hi] = { bg = 'None' }
+        end
+        ayu.setup {
+          mirage = true,
+          overrides = bg_overrides
+        }
+        ayu.colorscheme()
       end
     },
     -- git integration
     {
       'lewis6991/gitsigns.nvim',
-      cond = not vim.g.vscode,
       config = true,
     },
     -- multi cursor
     {
       'mg979/vim-visual-multi',
       branch = 'master',
-      cond = not vim.g.vscode,
       init = function()
         vim.g.VM_theme = 'codedark'
         vim.g.VM_maps = {
@@ -153,21 +175,6 @@ lazy.setup(
           ['Find Subword Under'] = '<C-d>',
         }
         vim.g.VM_set_statusline = 0
-      end
-    },
-    -- multi cursor in vscode
-    {
-      'vscode-neovim/vscode-multi-cursor.nvim',
-      event = 'VeryLazy',
-      cond = not not vim.g.vscode,
-      opts = {},
-      config = function()
-        vim.keymap.set(
-          { "n", "x", "i" }, "<C-d>",
-          function()
-            require("vscode-multi-cursor").addSelectionToNextFindMatch()
-          end
-        )
       end
     },
     -- toggle comment
@@ -181,7 +188,6 @@ lazy.setup(
     -- sudo handling
     {
       'lambdalisue/suda.vim',
-      cond = not vim.g.vscode,
       init = function()
         if not vim.opt.diff:get() then
           vim.g.suda_smart_edit = true
@@ -193,7 +199,6 @@ lazy.setup(
       'nvim-tree/nvim-tree.lua',
       version = "*",
       lazy = false,
-      cond = not vim.g.vscode,
       dependencies = {
         'nvim-tree/nvim-web-devicons',
       },
@@ -209,13 +214,11 @@ lazy.setup(
     -- editor config
     {
       "tpope/vim-sleuth",
-      cond = not vim.g.vscode,
     },
     -- indentation guide
     {
       "lukas-reineke/indent-blankline.nvim",
       main = 'ibl',
-      cond = not vim.g.vscode,
       opts = {
         indent = { char = '│' },
       },
@@ -223,37 +226,31 @@ lazy.setup(
     -- tab line
     {
       'romgrk/barbar.nvim',
-      cond = not vim.g.vscode,
       opts = {
         animation = false,
         auto_hide = true,
       },
-    },
-    -- scroll bar
-    {
-      'petertriho/nvim-scrollbar',
-      cond = not vim.g.vscode,
-      config = true,
     },
     -- tree sitter
     {
       'nvim-treesitter/nvim-treesitter',
       build = ':TSUpdate',
       main = 'nvim-treesitter.configs',
-      cond = not vim.g.vscode,
+      cond = is_not_large_file,
       opts = {
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
+        ignore_install = { 'help' },
       }
     },
     -- airline themes
     {
       'nvim-lualine/lualine.nvim',
       dependencies = {'nvim-tree/nvim-web-devicons'},
-      cond = not vim.g.vscode,
       opts = {
         options = {
+          theme = "onedark",
           component_separators = {left = '', right = ''},
           section_separators = {left = '', right = ''},
         },
@@ -262,77 +259,31 @@ lazy.setup(
     -- formatting
     {
       'vim-autoformat/vim-autoformat',
-      cond = not vim.g.vscode,
       config = function()
         noremap({'n', 'v'}, 'ff', ':Autoformat')
       end
     },
     -- LSP
     {
-      'VonHeikemen/lsp-zero.nvim',
-      branch = 'v3.x',
-      cond = not vim.g.vscode,
-      dependencies = {
-        'williamboman/mason.nvim',
-        'neovim/nvim-lspconfig',
-        'williamboman/mason-lspconfig.nvim',
-        'hrsh7th/nvim-cmp',
-        'hrsh7th/cmp-nvim-lsp',
-        'L3MON4D3/LuaSnip',
+      'saghen/blink.cmp',
+      version = '*',
+      cond = is_not_large_file,
+      opts = {
+        keymap = {
+          preset = "enter"
+        },
+        sources = {
+          default = { 'path', 'buffer' }
+        },
       },
-      config = function()
-        -- lsp_zero setup
-        local lsp_zero = require('lsp-zero')
-        lsp_zero.setup()
-        lsp_zero.on_attach(function(_, bufnr)
-          lsp_zero.default_keymaps { buffer = bufnr }
-        end)
-        -- mason & lspconfig setup
-        require('mason').setup {}
-        require('mason-lspconfig').setup {
-          handlers = {
-            lsp_zero.default_setup,
-            lua_ls = function()
-              local lua_opts = lsp_zero.nvim_lua_ls()
-              require('lspconfig').lua_ls.setup(lua_opts)
-            end,
-          },
-        }
-        -- cmp setup
-        local cmp = require('cmp')
-        local mapping = cmp.mapping
-        cmp.setup {
-          sources = {
-            { name = 'nvim_lsp' },
-            { name = 'buffer' },
-          },
-          format = lsp_zero.cmp_format(),
-          mapping = mapping.preset.insert {
-            -- Enter/Tab key to confirm completion
-            ['<cr>'] = mapping.confirm { select = false },
-            ['<tab>'] = mapping.confirm { select = false },
-            -- Scroll up and down in the completion documentation
-            ['<C-k>'] = mapping.scroll_docs(-4),
-            ['<C-j>'] = mapping.scroll_docs(4),
-          }
-        }
-      end
-    },
-    -- display issues
-    {
-      'folke/trouble.nvim',
-      dependencies = { "nvim-tree/nvim-web-devicons" },
-      cond = not vim.g.vscode,
-      config = true,
-    },
+      opts_extend = { "sources.default" }
+    }
   }, -- end of plugin list
   { -- lazy.nvim options
     install = {
       missing = true,
-      colorscheme = {'ayu'},
+      colorscheme = {'ayu-mirage'},
     },
   }
 )
-
-
 
