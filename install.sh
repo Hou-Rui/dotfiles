0c1342e9ee169b625b9e18db3461924b474394dc
Hou-Rui <houruinus@gmail.com>
2026-01-15 23:38:42 +0800

    add .Xresources

--- install.sh
+++ install.sh
@@ -25,6 +25,7 @@ home_ins_targets() {
       ('nvim') home_ins '.config/nvim'/**;;
       ('gtk4') REMOTE= home_ins '.config/gtk-4.0'/**;;
       ('kate') REMOTE= home_ins '.config/kate'/**;;
+      ('xres') REMOTE= home_ins '.Xresources';;
       (*) echo "Unknown target $target"; exit 1
     esac
   done
@@ -39,7 +40,7 @@ done
 shift $(($OPTIND - 1))
 
 if [ $# -eq 0 ]; then
-  home_ins_targets 'zsh' 'nvim' 'kate' 'gtk4'
+  home_ins_targets 'zsh' 'nvim' 'kate' 'xres' 'gtk4'
 else
   home_ins_targets "$@"
 fi
